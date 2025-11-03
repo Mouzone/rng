35c3458216ae86ed0bcd0650c4b9d0d0d3d9278d
Mouzone <sunnyliu010@gmail.com>
2025-11-02 20:02:10 -0500

    Add code for offsets for ranges and selection for with and without replacement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
-	let numbersToGenerate = $state(1);
-	let including = $state("including");
-	let minimum = $state(0);
-	let maximum = $state(0);
+	let numbersToGen = $state(1);
+	let incl = $state("including");
+	let withRep = $state("with replacement");
+	let min = $state(0);
+	let max = $state(0);
 	const results: Number[] = $state([]);
 
 	function generate() {
 		results.length = 0;
-		for (let i = 0; i < numbersToGenerate; i += 1) {
+		for (let i = 0; i < numbersToGen; i += 1) {
+			const offset = incl === "including" ? 1 : 0;
+			const effectiveMin = min + offset;
+			const effectiveMax = max - offset;
 			const result =
-				Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+				Math.floor(Math.random() * (effectiveMax - effectiveMin)) +
+				effectiveMin;
 			results.push(result);
 		}
 	}
@@ -19,20 +24,25 @@
 	Generate
 	<input
 		type="number"
-		bind:value={numbersToGenerate}
+		bind:value={numbersToGen}
 	/>
-	numbers between and
-	<select bind:value={including}
+	number(s)
+	<select bind:value={withRep}>
+		<option>with replacement</option>
+		<option>without replacement</option>
+	</select>
+	between and
+	<select bind:value={incl}
 		><option>including</option>
 		<option>not including</option></select
 	><input
 		type="number"
-		bind:value={minimum}
+		bind:value={min}
 	/>
 	and
 	<input
 		type="number"
-		bind:value={maximum}
+		bind:value={max}
 	/>
 </p>
 <div>
