a315c50c9f8272949ed3d915d9fa4f6df21fc9b6
Mouzone <sunnyliu010@gmail.com>
2025-11-04 14:31:08 -0500

    Get basic layout and input/select element sizing down

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -70,44 +70,81 @@
 	}
 </script>
 
-<p>
-	Generate
-	<input
-		inputmode="numeric"
-		bind:value={numbersToGenInput}
-		oninput={(e) => (numbersToGenInput = handleInput(e))}
-	/>
-	number(s)
-	<select bind:value={withRep}>
-		<option value="with replacement">with replacement</option>
-		<option value="without replacement">without replacement</option>
-	</select>
-	between and
-	<select bind:value={incl}>
-		<option value="including">including</option>
-		<option value="not including">not including</option>
-	</select>
-	<input
-		inputmode="numeric"
-		bind:value={minInput}
-		oninput={(e) => (minInput = handleInput(e))}
-	/>
-	and
-	<input
-		inputmode="numeric"
-		bind:value={maxInput}
-		oninput={(e) => (maxInput = handleInput(e))}
-	/>
-</p>
-<div style="display: flex; gap: 1em;">
-	{#each results as result}
-		<p>{result}</p>
-	{/each}
+<div id="page">
+	<div id="generator">
+		<p>
+			Generate
+			<input
+				inputmode="numeric"
+				bind:value={numbersToGenInput}
+				oninput={(e) => (numbersToGenInput = handleInput(e))}
+			/>
+			number(s)
+			<select bind:value={withRep}>
+				<option value="with replacement">with replacement</option>
+				<option value="without replacement">without replacement</option>
+			</select>
+			between and
+			<select bind:value={incl}>
+				<option value="including">including</option>
+				<option value="not including">not including</option>
+			</select>
+			<input
+				inputmode="numeric"
+				bind:value={minInput}
+				oninput={(e) => (minInput = handleInput(e))}
+			/>
+			and
+			<input
+				inputmode="numeric"
+				bind:value={maxInput}
+				oninput={(e) => (maxInput = handleInput(e))}
+			/>
+		</p>
+
+		<button
+			onclick={generate}
+			{disabled}
+		>
+			Generate
+		</button>
+	</div>
+
+	<div style="display: flex; gap: 1em;">
+		{#each results as result}
+			<p>{result}</p>
+		{/each}
+	</div>
 </div>
 
-<button
-	onclick={generate}
-	{disabled}
->
-	Generate
-</button>
+<style>
+	#page {
+		height: 100dvh;
+		width: 100dvw;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	#generator {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	p {
+		text-align: center;
+	}
+	input,
+	select {
+		text-align: center;
+		margin: 0, 1em, 1em, 0;
+	}
+	input {
+		width: 2em;
+	}
+	select {
+		width: 12em;
+	}
+</style>
