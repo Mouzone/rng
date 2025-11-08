a1a681d80688d3cd1b39bdbad8339d845767e4f9
Mouzone <sunnyliu010@gmail.com>
2025-11-08 15:50:28 -0500

    Add pwa code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,25 @@
 <script lang="ts">
 	import "normalize.css";
 	import favicon from "$lib/assets/favicon.svg";
+	import { onMount } from "svelte";
 
+	async function detectSWUpdate() {
+		const registration = await navigator.serviceWorker.ready;
+
+		registration.addEventListener("updatefound", () => {
+			const newSW = registration.installing;
+			newSW?.addEventListener("statechange", () => {
+				if (newSW.state === "installed") {
+					if (confirm("New update available! Reload to update?")) {
+						newSW.postMessage({ type: "SKIP_WAITING" });
+						window.location.reload();
+					}
+				}
+			});
+		});
+	}
+
+	onMount(() => detectSWUpdate());
 	let { children } = $props();
 </script>
 
