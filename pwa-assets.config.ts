feb77a3bd102448b52f0cbcd167afde97acf2477
Mouzone <sunnyliu010@gmail.com>
2025-11-11 15:01:50 -0500

    Fix icon generator for apple and add necessary script to npm

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,16 +1,24 @@
-import type { Preset } from "@vite-pwa/assets-generator/config";
+import { defineConfig, type Preset } from "@vite-pwa/assets-generator/config";
 
-export const minimal2023Preset: Preset = {
+const noPaddingPreset: Preset = {
 	transparent: {
 		sizes: [64, 192, 512],
 		favicons: [[48, "favicon.ico"]],
+		padding: 0,
 	},
+
 	maskable: {
 		sizes: [512],
 		padding: 0,
 	},
+
 	apple: {
 		sizes: [180],
 		padding: 0,
 	},
 };
+
+export default defineConfig({
+	preset: noPaddingPreset,
+	images: "static/icons/logo.png",
+});
