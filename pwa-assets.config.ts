99ffbe2aa08eab30bdef75026a91f8f639376699
Mouzone <sunnyliu010@gmail.com>
2025-11-11 14:46:17 -0500

    Add assets generator to generate the apple icon

---
+++ pwa-assets.config.ts
@@ -0,0 +1,16 @@
+import type { Preset } from "@vite-pwa/assets-generator/config";
+
+export const minimal2023Preset: Preset = {
+	transparent: {
+		sizes: [64, 192, 512],
+		favicons: [[48, "favicon.ico"]],
+	},
+	maskable: {
+		sizes: [512],
+		padding: 0,
+	},
+	apple: {
+		sizes: [180],
+		padding: 0,
+	},
+};
